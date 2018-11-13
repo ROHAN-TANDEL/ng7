aad30af9db9990baef0b4e2dd95dea87ff638b62
rohan <rohan.t@scriptbees.com>
2018-11-13 14:41:45 +0530

    data added and updated.

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -8,19 +8,8 @@ import { ArticleListService } from './article-list.service';
 export class ArticleListComponent implements OnInit {
 
   constructor(private serv_article_list: ArticleListService) { }
-  article_resource: any;
-  a: any =  ["id", "title", "body", "author", "editor", "active", "created_at", "updated_at"];
-  b: any = {1:1, 2:2, 3:3, 4:4, 5:5, 6:6};
   article_array_keys: any[];
-  article_array_values: any[];
-  
-  ngOnInit() {
-    this.bindArticles();
-  }
-
-  bindArticles() {
-
-    this.article_resource = {
+  article_resource = {
       "id": 11,
       "title": "Hello data updated even now",
       "body": "Ducimus et repellendus eveniet ab nihil labore. Autem in nulla vel rerum sit ut omnis. Nulla est sunt minus. Dolores sapiente totam consequuntur omnis officiis voluptas ut.",
@@ -31,17 +20,25 @@ export class ArticleListComponent implements OnInit {
       "updated_at": "2018-09-04 11:27:22"
     };
 
+  ngOnInit() {
 
-     // this.article_resource['request_type'] = 'get';
-    // this.article_resource['request_type'] = 'patch';
-    //  this.article_resource['request_type'] = 'delete';
+    this.bindArticles(this.article_array_keys);
+  }
 
+  bindArticles(new_article_resource) {
+
+    this.article_resource = Object.assign(this.article_resource,new_article_resource);
+  
     this.serv_article_list.getFullArticleList(this.article_resource['request_type'], this.article_resource)
       .subscribe(
         (article_list_data: any) => { 
-          console.log(article_list_data);
-          this.article_array_keys = Array.of(article_list_data);//Object.keys(article_list_data)
-          // this.article_array_values = Object.values(article_list_data)
+          console.log('non converted array', article_list_data);
+          this.article_array_keys = article_list_data;
         });
+  }
+  item_editable(id) {
+    this.article_resource['id'] = id;
+    this.article_resource['request_type'] = 'get';
+    this.bindArticles(this.article_resource);
   }
 }
